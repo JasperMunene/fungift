68ae3b711960f8907786a78defda49f5e904e72b
JasperMunene <jaspermunene2022@gmail.com>
2025-08-28 08:55:31 +0300

    changes

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -87,7 +87,7 @@ const Product: React.FC<ProductProps> = ({ data, type, style }) => {
         const slugify = (text: string) =>
             text.toLowerCase().replace(/\s+/g, "-");
         // redirect to shop with category selected
-        router.push(`/shop/gift-cards/${slugify(data.name)}`);
+        router.push(`/shop/gifts/${slugify(data.name)}`);
     };
 
     let percentSale = Math.floor(100 - ((data.price / data.originPrice) * 100))
